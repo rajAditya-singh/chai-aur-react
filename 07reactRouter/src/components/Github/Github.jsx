6879311d920c28559428f1f5ca5f30fa10a25008
Qwerty@126 <indrmy.126@gmail.com>
2024-09-20 11:59:48 +0530

    s

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
-
+import { useLoaderData } from 'react-router-dom'
 function Github() {
-  const [data, setData] = useState([])
-  useEffect(()=>{
-    fetch('https://api.github.com/users/hiteshchoudhary')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      setData(data)
-    })
-  }, [])
+  const data = useLoaderData()
+  // const [data, setData] = useState([])
+  // useEffect(()=>{
+  //   fetch('https://api.github.com/users/hiteshchoudhary')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data);
+  //     setData(data)
+  //   })
+  // }, [])
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
       Github Followers: {data.followers}
@@ -20,3 +21,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubinfoLoader = async()=>{
+  const response =  await fetch('https://api.github.com/users/hiteshchoudhary')
+  return response.json
+}
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -7,7 +7,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github from './components/Github/Github.jsx'
+import Github, { githubinfoLoader } from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -37,7 +37,11 @@ const router = createBrowserRouter (
     <Route path='about' element = {<About />}/>
     <Route path='contact' element = {<Contact />}/>
     <Route path='user/:userId' element = {<User />}/>
-    <Route path='github' element = {<Github />}/>
+    <Route 
+    loader = {githubinfoLoader}
+    path='github' 
+    element = {<Github />}
+    />
     </Route>
   )
 )
