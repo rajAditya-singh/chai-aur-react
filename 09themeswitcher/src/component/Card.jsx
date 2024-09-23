b9f291ac07e3e9ef76621ea26c8f7cc4f40bf196
Qwerty@126 <indrmy.126@gmail.com>
2024-09-23 14:41:12 +0530

    s

--- 09themeswitcher/src/component/Card.jsx
+++ 09themeswitcher/src/component/Card.jsx
@@ -5,25 +5,28 @@ export default function Card() {
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow 
         dark:bg-customNavy dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="image/dp.png" alt="product_image1" />
+                <img className="p-8 rounded-full" src="image/dp.png" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        ADITYA RAJ SINGH || FRONT-END DEVLOPER
+                        ADITYA RAJ SINGH 
+                        <h5 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-customSky'>
+                            FRONT-END DEVLOPER
+                        </h5> 
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
 
                     <span>
-                        <span className=" text-black text-xs font-extrabold mr-2 px-3 py-0.5 rounded  dark:text-customSky ml-3">SKILLS</span>
+                        <span className=" text-black text-l font-extrabold mr-2 px-3 py-0.5 rounded  dark:text-customSky ml-3">SKILLS</span>
                         <br />
                         <ol className="grid grid-cols-3 gap-3 pl-5">
                             <li className="text-black text-xs font-semibold px-3 py-0.5 rounded dark:text-white">
-                                CSS (Tailwind CSS)
+                                TAILWIND(CSS)
                             </li>
                             <li className="text-black text-xs font-semibold px-3 py-0.5 rounded dark:text-white">
-                                JAVASCRIPT (REACT JS)
+                                JAVASCRIPT(REACT)
                             </li>
                             <li className="text-black text-xs font-semibold px-3 py-0.5 rounded dark:text-white">
                                 SQL
@@ -45,7 +48,7 @@ export default function Card() {
                     <a
                         href='image/Resume.pdf'
                         download
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                         DOWNLOAD RESUME
                     </a>
