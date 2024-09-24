b6ee0672c3dc1edd600e04bdbd08e17d2b4c02c5
Qwerty@126 <indrmy.126@gmail.com>
2024-09-24 10:10:00 +0530

    s

--- 09themeswitcher/src/component/Card.jsx
+++ 09themeswitcher/src/component/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 
 export default function Card() {
     return (
@@ -11,10 +11,10 @@ export default function Card() {
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                         <h5 className='hover:scale-110'>ADITYA RAJ SINGH</h5>
-                             
+
                         <h5 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-customSky hover:scale-110'>
                             FRONT-END DEVLOPER
-                        </h5> 
+                        </h5>
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
@@ -23,7 +23,7 @@ export default function Card() {
                         <span className=" text-black text-l font-bold mr-2 px-3 py-0.5 rounded  dark:text-customSky ml-3"><h6 className=' hover:scale-110 '>SKILLS</h6></span>
                         <ol className="grid grid-cols-3 gap-4 p-2">
                             <li className="text-black text-sm font-semibold px-3 py-0.5 rounded dark:text-white hover:scale-110">
-                                HTML CSS 
+                                HTML CSS
                             </li>
                             <li className="text-black text-sm font-semibold px-3 py-0.5 rounded dark:text-white hover:scale-110">
                                 JAVASCRIPT
@@ -51,15 +51,15 @@ export default function Card() {
                 </div>
                 <div className="flex items-center justify-between">
                     <div className="flex items-center justify-center gap-5">
-                    <a href="https://github.com/rajAditya-singh?tab=repositories" >
-                     <img src="image/git.png" alt="" width="25px" height="25px" className='hover:scale-110'></img>
-                    </a>
-                    <a href="https://www.linkedin.com/in/aditya-raj-singh-20b5a6313/"> 
-                    <img src="image/link.png" alt="" width="30px" height="30px" className='hover:scale-110'></img>
-                    </a>
-                    <a href="https://www.instagram.com/_aaddii_tya_/">
-                    <img src="image/insta.png" alt="" width="25px" height="25px" className='hover:scale-110'></img>
-                    </a>
+                        <a href="https://github.com/rajAditya-singh?tab=repositories" >
+                            <img src="image/git.png" alt="" width="25px" height="25px" className='hover:scale-110'></img>
+                        </a>
+                        <a href="https://www.linkedin.com/in/aditya-raj-singh-20b5a6313/">
+                            <img src="image/link.png" alt="" width="30px" height="30px" className='hover:scale-110'></img>
+                        </a>
+                        <a href="https://www.instagram.com/_aaddii_tya_/">
+                            <img src="image/insta.png" alt="" width="25px" height="25px" className='hover:scale-110'></img>
+                        </a>
                     </div>
                     <a
                         href='image/Resume.pdf'
