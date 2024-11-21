dc9ecfa73cf8b801fbbbe0274d7241934e479731
Qwerty@126 <indrmy.126@gmail.com>
2024-11-21 16:23:39 +0530

    s

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -8,7 +8,7 @@ function customRender(reactElement, container){
         const domElement = document.createElement(reactElement.type)
         domElement.innerHTML = reactElement.children
         for ( const prop in reactElement.props){
-            if (prop == 'children') continue;
+            if (prop === 'children') continue;
             domElement.setAttribute(prop, reactElement.props[prop])
         }
         container.appendChild(domElement)
