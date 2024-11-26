03814eca54927f3c92212dfa861ed7b7539ee52f
Qwerty@126 <indrmy.126@gmail.com>
2024-11-26 16:29:24 +0530

    s

--- 05passgen/src/App.jsx
+++ 05passgen/src/App.jsx
@@ -26,70 +26,69 @@ function App() {
 
   }, [length, numallow, charallow, setpassword])
 
-  const copyPasswordToClipBorad = useCallback(()=>{
+  const copyPasswordToClipBorad = useCallback(() => {
     passref.current?.select();
     passref.current?.setSelectionRange(0, 20)
     window.navigator.clipboard.writeText(password)
   }, [password])
 
-  useEffect(()=>{
+  useEffect(() => {
     passwordGenerator()
   }, [length, numallow, charallow, passwordGenerator])
   return (
-
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-    <h1 className='text-white text-center my-3'>Password generator</h1>
-  <div className="flex shadow rounded-lg overflow-hidden mb-4">
-      <input
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
           type="text"
           value={password}
           className="outline-none w-full py-1 px-3"
           placeholder="Password"
           readOnly
           ref={passref}
-      />
-      <button
-      onClick={copyPasswordToClipBorad}
-      className='cursor-pointer hover:bg-sky-300 outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
-      >copy</button>
-      
-  </div>
-  <div className='flex text-sm gap-x-2'>
-    <div className='flex items-center gap-x-1'>
-      <input 
-      type="range"
-      min={6}
-      max={100}
-      value={length}
-       className='cursor-pointer'
-       onChange={(e) => {setlength(e.target.value)}}
         />
-        <label>Length: {length}</label>
-    </div>
-    <div className="flex items-center gap-x-1">
-    <input
-        type="checkbox"
-        defaultChecked={numallow}
-        id="numberInput"
-        onChange={() => {
-            setnumallow((prev) => !prev);
-        }}
-    />
-    <label htmlFor="numberInput">Numbers</label>
-    </div>
-    <div className="flex items-center gap-x-1">
-        <input
+        <button
+          onClick={copyPasswordToClipBorad}
+          className='cursor-pointer hover:bg-sky-300 outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className='cursor-pointer'
+            onChange={(e) => { setlength(e.target.value) }}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={numallow}
+            id="numberInput"
+            onChange={() => {
+              setnumallow((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
             type="checkbox"
             defaultChecked={charallow}
             id="characterInput"
             onChange={() => {
-                setcharallow((prev) => !prev )
+              setcharallow((prev) => !prev)
             }}
-        />
-        <label htmlFor="characterInput">Characters</label>
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   )
 }
 
