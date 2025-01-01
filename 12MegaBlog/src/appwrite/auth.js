1a66ab5feb8ef118c1025c1ddb6a29678817bf24
Qwerty@126 <indrmy.126@gmail.com>
2025-01-01 17:17:06 +0530

    s

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,19 +1,63 @@
-import conf from '../conf.js';
+import conf from '../conf/conf.js';
 import { Client, Account, ID } from "appwrite";
+
 
 export class AuthService {
     client = new Client();
     account;
 
-    constructor(){
+    constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId)
-        this.account =new Account(this.client)    
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+
     }
 
-    
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.logIn({email, password});
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async logIn ({email, password}){
+        try {
+            return await this.account.createEmailSession(
+                email,
+                password
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+        return null
+    } 
+
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+    }
 }
-const authService = new AuthService()
+
+const authService = new AuthService();
 
 export default authService
+
